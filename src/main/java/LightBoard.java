e618d10b194aa43d8e7a343683781d3278b1e74c
cassylee-7 <cassandrasyllee@gmail.com>
2025-04-29 10:24:50 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -11,7 +11,16 @@ public class LightBoard
   public LightBoard(int numRows, int numCols)
   {
     /* to be implemented in part (a) */
-
+    lights = new boolean [numRows][numCols];
+    for (int r = 0; r < lights.length; r++) {
+      for (int c = 0; c < lights[r].length; c++) {
+        double prob = Math.random();
+        if (prob < 0.4)
+          lights [r][c] = true;
+        else
+          lights [r][c] = false;
+      }
+    }
   }
 
   /** Evaluates a light in row index row and column index col and returns a status
@@ -21,7 +30,18 @@ public class LightBoard
   public boolean evaluateLight(int row, int col)
   {
     /* to be implemented in part (b) */
-   
+    int countOn = 0;
+    int r = lights.length;
+    for (int c = 0; c < lights[r].length; c++) {
+      if (lights[row][c] == true)
+        countOn++;
+    }
+    if (lights[row][col] && countOn % 2 == 0)
+      return false;
+    if (!lights[row]col] && countOn % 3 == 0)
+      return true;
+    else
+      return lights[row][col];
  
   }
   public boolean[][] getLights()
